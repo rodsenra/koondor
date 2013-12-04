9301011c3444854f4612a98dc27eddaca18561f1
rodsenra <rodsenra@gmail.com>
2013-12-04 10:02:36 -0200

    refactoring directory names and added Makefile

--- sample/get_news.py
+++ sample/get_news.py
@@ -1,16 +1,21 @@
+import os
 import sys
 import codecs
 import ujson
 import requests
 
-file_data = open(sys.argv[1], "rb").read()
+input_json = sys.argv[1]
+dir_with_news_html = sys.argv[2]
+
+file_data = open(input_json, "rb").read()
 news_meta = ujson.loads(file_data)['hits']['hits']
 url_and_date = [(i['_source']['modified'], i['_id']) for i in news_meta]
 sorted_urls = sorted(url_and_date)
 for date, url in sorted_urls:
     print("Downloading " + url)
     response = requests.get(url, timeout=120)
-    outfilename = "{0}.html".format(date)
+    outfilename = os.path.join(dir_with_news_html, "{0}.html".format(date))
+
     if response.status_code == 200:
         codecs.open(outfilename, "wb", encoding=response.encoding).write(response.text)
     else:
--- sample/html2txt.py
+++ sample/html2txt.py
@@ -1,12 +1,23 @@
 import codecs
-from glob import glob 
+from glob import glob
 import os
 import sys
 from bs4 import BeautifulSoup
 
-dirpath = os.path.join(sys.argv[1], "*.html")
+dir_with_news_html = sys.argv[1]
+dir_with_news_txt = sys.argv[2]
+
+dirpath = os.path.join(dir_with_news_html, "*.html")
 for filename in glob(dirpath):
-	html_file = codecs.open(filename, "r", encoding="utf-8")
-	soup = BeautifulSoup(html_file.read())
-        print soup.find_all('div', class_='materia-conteudo')
-	html_file.close()
+    html_file = codecs.open(filename, "r", encoding="utf-8")
+    soup = BeautifulSoup(html_file.read())
+    try:
+        print "Converting " + filename
+        txt_content = soup.find_all('div', class_='materia-conteudo')[0].text
+        txt_content = txt_content.replace('\n', ' ')
+        txt_file = os.path.join(dir_with_news_txt, os.path.basename(filename))
+        txt_file = txt_file.replace('.html', '.txt')
+        codecs.open(txt_file, "wb", encoding="utf-8").write(txt_content)
+    except IndexError:
+        pass
+    html_file.close()
